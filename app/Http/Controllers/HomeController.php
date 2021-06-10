4a4a3c173bab4f13c49479fa5025b1d294900a75
Michele Lorusso <kirito102095@hotmail.it>
2021-06-10 18:16:42 +0200

    Complete

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,6 +10,10 @@ class HomeController extends Controller
     public function index() {
         $movies = Movie::all();
 
-        return view('home');
+        $data = [
+            'movies' => $movies,
+         ];
+
+        return view('home', $data);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,11 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Cinema</title>
+
+    {{-- CSS --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
     
-    <h1>Homepage gedfg</h1>
+    <div class="logo">
+        <img src="{{asset("img/Cinema-paradiso.png")}}" alt="cinema">
+    </div>
+
+    <div class="container">
+        <h3>Film in disponibili in sala:</h3>
+    
+        <ul>
+            @foreach ($movies as $movie)
+            
+                <li>
+                    <div class="box">
+                        <span class="name">Title: </span><span class="title info">{{$movie['title']}}</span>
+                    </div>
+                    <div class="box">
+                        <span class="name">Original-Title: </span><span class="info">{{$movie['original_title']}}</span>
+                    </div>
+                    <div class="box">
+                        <span class="name">Nationality: </span><span class="info">{{$movie['nationality']}}</span>
+                    </div>
+                    <div class="box">
+                        <span class="name">Date: </span><span class="info">{{$movie['date']}}</span>
+                    </div>
+                    <div class="box">
+                        <span class="name">Vote: </span><span class="info vote">{{$movie['vote']}}</span>
+                    </div>
+                </li>
+
+            @endforeach
+        </ul>
+
+    </div>
+    
 
 </body>
 </html>
